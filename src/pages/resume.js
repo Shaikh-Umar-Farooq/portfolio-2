e7e803fdaad14ca3f7551de77c406b67c9cdbf33
Shaikh-Umar-Farooq <smuf7080@gmail.com>
2022-11-01 23:35:09 +0530

    resume added

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,14 +1,26 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
+import resume from "../images/Resume.svg";
+import pdf from "../images/Resume.pdf";
+import "../pages/css/resume.css";
 
 const Resume = () => {
     return (
-        <>
+        <div className="resume">
             <Navbar />
-            <section className="hero-section">
-                <p>Welcome to </p>
-                <h1>Thapa Technical Service Page</h1>
-            </section>
-        </>
+            <div className="resume-container">
+                <img src={resume} alt="resume" />
+            </div>
+            <a href={pdf} download="umarshaikh-resume">
+                <button
+                    className="download"
+                >
+                    Download
+                </button>
+            </a>
+
+            <Footer />
+        </div>
     );
 };
 
